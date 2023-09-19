ee09ea23665c3fe7abb0f369a3f38b5695a963cf
Larry Clement <lclement@calbaptist.edu>
2023-09-19 04:14:25 -0700

    Prior to Class (Week 3)

--- Mirror/src/Mirror.java
+++ Mirror/src/Mirror.java
@@ -1,5 +1,5 @@
 public class Mirror {
-    public static final int SIZE = 4;
+    public static final int SIZE = 3;
 
     public static void main(String[] args) {
         border();
@@ -13,10 +13,40 @@ public class Mirror {
     }
 
     public static void topHalf() {
-        System.out.println("topHalf");
+        for (int line = 1; line <= SIZE; line++) {
+            System.out.print("|");
+            for(int i = 1; i <= -2 * line + 2 * SIZE; i++) {
+                System.out.print(" ");
+            }
+            System.out.print("<>");
+            for(int i = 1; i <= 4 * line - 4; i++) {
+                System.out.print(".");
+            }
+            System.out.print("<>");
+
+            for(int i = 1; i <= -2 * line + 2 * SIZE; i++) {
+                System.out.print(" ");
+            }
+            System.out.println("|");
+        }
     }
 
     public static void bottomHalf() {
-        System.out.println("bottomHalf");
+        for (int line = SIZE; line >= 1; line--) {
+            System.out.print("|");
+            for(int i = 1; i <= -2 * line + 2 * SIZE; i++) {
+                System.out.print(" ");
+            }
+            System.out.print("<>");
+            for(int i = 1; i <= 4 * line - 4; i++) {
+                System.out.print(".");
+            }
+            System.out.print("<>");
+
+            for(int i = 1; i <= -2 * line + 2 * SIZE; i++) {
+                System.out.print(" ");
+            }
+            System.out.println("|");
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 09.19.A.InClassExamples
 
-In class examples for the week of 09.19 - 09.21.2023
+In class examples for section "A" for the week of 09.19 - 09.21.2023
